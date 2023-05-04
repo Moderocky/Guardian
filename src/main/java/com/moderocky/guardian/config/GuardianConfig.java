e25d29656debdb9f1c7975b683ee7304baedf165
Moderocky <admin@moderocky.com>
2023-05-04 12:25:27 +0100

    Improve guardian config.

--- src/main/java/com/moderocky/guardian/config/GuardianConfig.java
+++ src/main/java/com/moderocky/guardian/config/GuardianConfig.java
@@ -1,131 +1,144 @@
 package com.moderocky.guardian.config;
 
 import com.moderocky.guardian.Guardian;
-import com.moderocky.mask.annotation.Configurable;
-import com.moderocky.mask.gui.ItemFactory;
-import com.moderocky.mask.mirror.Mirror;
-import com.moderocky.mask.template.Config;
+import mx.kenzie.fern.Fern;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.permissions.PermissionDefault;
 import org.bukkit.persistence.PersistentDataType;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Arrays;
-import java.util.Collection;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Collections;
+import java.util.List;
 
-public class GuardianConfig implements Config {
+public class GuardianConfig {
 
-    @Configurable("wand")
     public boolean enableWand = true;
 
-    @Configurable.Comment("Any Bukkit Material enum is permitted.")
-    @Configurable("wand")
     public Material wandMaterial = Material.DEBUG_STICK;
 
-    @Configurable.Comment("Allows a custom model data value to be used for the wand.")
-    @Configurable("wand")
-    @Configurable.Bounded(minValue = 0, maxValue = 999999999)
     public int wandModelData = 102;
 
-    @Configurable.Comment("Any Bukkit Material enum is permitted.")
-    @Configurable("wand")
     public Material polywandMaterial = Material.DEBUG_STICK;
 
-    @Configurable.Comment("Allows a custom model data value to be used for the wand.")
-    @Configurable("wand")
-    @Configurable.Bounded(minValue = 0, maxValue = 999999999)
     public int polywandModelData = 103;
 
-    @Configurable.Comment("Use '%s' to indicate the position number.")
-    @Configurable("wand")
     public String setPosition = "Set Zone Position #%s";
 
-    @Configurable("wand")
     public String clearPosition = "Cleared Zone Positions";
 
-    @Configurable.Comment({
-            "The maximum diameter for a player-created zone.",
-            "This goes from corner to corner."
-    })
-    @Configurable("zone")
-    @Configurable.Bounded(minValue = 1, maxValue = 100000)
     public long maxZoneDiameter = 128;
 
-    @Configurable("zone")
     public String actionDenyMessage = "§bYou cannot interact with this zone!";
 
-    @Configurable.Comment({"Allows use of the zone enter/exit flags.", "Might affect server performance if misused."})
-    @Configurable("settings")
     public boolean checkEntryExit = true;
 
-    @Configurable.Comment("Allowed values are TRUE, OP, NOT_OP and FALSE.")
-    @Configurable("settings")
     public PermissionDefault allowBasicFlags = PermissionDefault.TRUE;
 
-    @Configurable.Comment("Allowed values are TRUE, OP, NOT_OP and FALSE.")
-    @Configurable("settings")
     public PermissionDefault allowSpecialFlags = PermissionDefault.OP;
 
-    @Configurable.Comment("Delay between saving to file - in seconds.")
-    @Configurable("settings")
-    @Configurable.Bounded(minValue = 10, maxValue = 100000)
     public long saveDelay = 100;
 
-    @Configurable.Comment({
-            "Delay between clearing action caches (used to speed up the plugin) - in seconds.",
-            "Longer delays will increase performance, but have the potential to cause false blocking."
-    })
-    @Configurable("settings")
     public long actionCacheResetDelay = 30;
 
-    @Configurable("settings")
-    @Configurable.Comment("Compress the stored zone data?")
     public boolean compressData = true;
 
     public GuardianConfig() {
-        load();
+        this.load();
     }
 
-    @Override
+    private void load() {
+        final File file = new File(this.getFolderPath() + this.getFileName());
+        if (!file.exists()) return;
+        try (Fern fern = new Fern(new FileInputStream(file))) {
+            fern.toObject(this);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void save() {
+        final File file = new File(this.getFolderPath() + this.getFileName());
+        if (file.getParentFile() != null && !file.getParentFile().exists()) file.getParentFile().mkdirs();
+        try {
+            if (!file.exists()) file.createNewFile();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        try (Fern fern = new Fern(new FileOutputStream(file))) {
+            fern.write(this);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
     public @NotNull String getFolderPath() {
         return "plugins/Guardian/";
     }
 
-    @Override
     public @NotNull String getFileName() {
-        return "config.yml";
+        return "config.fern";
     }
 
     public ItemStack getWand() {
-        return new ItemFactory(wandMaterial, 1, itemMeta -> {
-            itemMeta.setCustomModelData(wandModelData);
-            itemMeta.setDisplayName("§6Zone Wand");
-            itemMeta.setLore(Arrays.asList("§7LClick to set Pos #1", "§7RClick to set Pos #2"));
-            itemMeta.addEnchant(Enchantment.VANISHING_CURSE, 1, true);
-            if (Mirror.classExists("co.aikar.timings.Timing"))
-                new Mirror<>(itemMeta).method("setDestroyableKeys", Collection.class)
-                        .invoke(Collections.emptyList());
-            itemMeta.getPersistentDataContainer().set(Guardian.getNamespacedKey("wand"), PersistentDataType.BYTE, (byte) 1);
-            itemMeta.addItemFlags(ItemFlag.values());
-        }).create();
+        final ItemStack stack = new ItemStack(wandMaterial, 1);
+        final ItemMeta meta = stack.getItemMeta();
+        meta.setCustomModelData(wandModelData);
+        meta.displayName(Component.text("Zone Wand", NamedTextColor.LIGHT_PURPLE).decoration(TextDecoration.ITALIC, false));
+        meta.lore(List.of(
+            Component.textOfChildren(
+                Component.text("Pos #1: ", NamedTextColor.GRAY),
+                Component.keybind("key.attack", NamedTextColor.AQUA)
+            ).decoration(TextDecoration.ITALIC, false),
+            Component.textOfChildren(
+                Component.text("Pos #2: ", NamedTextColor.GRAY),
+                Component.keybind("key.use", NamedTextColor.AQUA)
+            ).decoration(TextDecoration.ITALIC, false)
+        ));
+        meta.addEnchant(Enchantment.VANISHING_CURSE, 1, true);
+        meta.setDestroyableKeys(Collections.emptyList());
+        meta.getPersistentDataContainer().set(Guardian.getNamespacedKey("wand"), PersistentDataType.BYTE, (byte) 1);
+        meta.addItemFlags(ItemFlag.values());
+        stack.setItemMeta(meta);
+        return stack;
     }
 
     public ItemStack getPolywand() {
-        return new ItemFactory(polywandMaterial, 1, itemMeta -> {
-            itemMeta.setCustomModelData(polywandModelData);
-            itemMeta.setDisplayName("§6Zone Polywand");
-            itemMeta.setLore(Arrays.asList("§7LClick to add a vertex", "§7RClick to reset vertices"));
-            itemMeta.addEnchant(Enchantment.VANISHING_CURSE, 1, true);
-            if (Mirror.classExists("co.aikar.timings.Timing"))
-                new Mirror<>(itemMeta).method("setDestroyableKeys", Collection.class)
-                        .invoke(Collections.emptyList());
-            itemMeta.getPersistentDataContainer().set(Guardian.getNamespacedKey("polywand"), PersistentDataType.BYTE, (byte) 1);
-            itemMeta.addItemFlags(ItemFlag.values());
-        }).create();
+        final ItemStack stack = new ItemStack(polywandMaterial, 1);
+        final ItemMeta meta = stack.getItemMeta();
+        meta.setCustomModelData(polywandModelData);
+        meta.displayName(Component.text("Zone Polywand", NamedTextColor.LIGHT_PURPLE).decoration(TextDecoration.ITALIC, false));
+        meta.lore(List.of(
+            Component.textOfChildren(
+                Component.text("Add vertex: ", NamedTextColor.GRAY),
+                Component.keybind("key.attack", NamedTextColor.AQUA)
+            ).decoration(TextDecoration.ITALIC, false),
+            Component.textOfChildren(
+                Component.text("Clear vertices: ", NamedTextColor.GRAY),
+                Component.keybind("key.use", NamedTextColor.AQUA)
+            ).decoration(TextDecoration.ITALIC, false)
+        ));
+        meta.addEnchant(Enchantment.VANISHING_CURSE, 1, true);
+        meta.setDestroyableKeys(Collections.emptyList());
+        meta.getPersistentDataContainer().set(Guardian.getNamespacedKey("polywand"), PersistentDataType.BYTE, (byte) 1);
+        meta.addItemFlags(ItemFlag.values());
+        stack.setItemMeta(meta);
+        return stack;
+    }
+
+    public void reload() {
+        this.load();
     }
 
 }
